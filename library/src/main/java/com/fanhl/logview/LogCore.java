2594d26d0b32a52aecb88d83496bccfa74807df1
fanhl <Dyguest0315>
2016-05-04 20:42:14 +0800

    logCore

--- library/src/main/java/com/fanhl/logview/LogCore.java
+++ library/src/main/java/com/fanhl/logview/LogCore.java
@@ -1,10 +1,51 @@
 package com.fanhl.logview;
 
+import com.fanhl.logview.model.LogFilter;
+import com.fanhl.logview.model.LogItem;
+import com.fanhl.logview.util.Stabilizer;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * The core of log.Just logic.No relationship of ui.
  * <p/>
  * Created by fanhl on 16/5/3.
  */
 public class LogCore {
+    private static final int LIMIT_LENGTH = 100;
 
+    private static final Stabilizer stabilizer;
+
+    private static final List<LogItem> fullLogs;
+    private static final List<LogItem> filterdLogs;
+    private static final List<LogItem> bufferLogs;
+
+    private static LogFilter logFilter;
+
+    static {
+        stabilizer = new Stabilizer();
+
+        fullLogs = Collections.synchronizedList(new LinkedList<LogItem>());
+        filterdLogs = Collections.synchronizedList(new LinkedList<LogItem>());
+        bufferLogs = Collections.synchronizedList(new LinkedList<LogItem>());
+
+        logFilter = new LogFilter();
+    }
+
+    public void addLog(LogItem logItem) {
+        addBufferLog(logItem);
+
+        if (!stabilizer.check()) return;
+        stabilizer.actived();
+
+
+    }
+
+    private void addBufferLog(LogItem logItem) {
+        bufferLogs.add(logItem);
+        int overflowCount = bufferLogs.size() - LIMIT_LENGTH;
+        if (overflowCount > 0) bufferLogs.removeAll(bufferLogs.subList(0, overflowCount));
+    }
 }
---
+++ library/src/main/java/com/fanhl/logview/model/LogFilter.java
@@ -0,0 +1,7 @@
+package com.fanhl.logview.model;
+
+/**
+ * Created by fanhl on 16/5/4.
+ */
+public class LogFilter {
+}
