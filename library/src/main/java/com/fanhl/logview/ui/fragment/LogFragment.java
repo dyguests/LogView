60f01709c6d8d68dc3c660d26e5c718056b4eb04
fanhl <Dyguest0315>
2016-05-05 09:07:36 +0800

    logFragment

--- library/src/main/java/com/fanhl/logview/ui/fragment/LogFragment.java
+++ library/src/main/java/com/fanhl/logview/ui/fragment/LogFragment.java
@@ -1,5 +1,6 @@
 package com.fanhl.logview.ui.fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,12 +8,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.fanhl.logview.R;
+
 /**
  * Created by fanhl on 16/5/5.
  */
 public class LogFragment extends Fragment {
-    @Nullable @Override
+    @Nullable
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.logview_fragment_log, container, false);
+        assignViews(view);
+        initData();
+        refreshData();
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        LogViewCore.registerLogFragment(this);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        LogViewCore.unregisterLogFragment(this);
+    }
+
+    private void assignViews(View view) {
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void refreshData() {
+
     }
 }
