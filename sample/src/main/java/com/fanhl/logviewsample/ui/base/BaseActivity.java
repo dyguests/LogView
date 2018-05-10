b05a35d36ae7664502b3a8526fa5de63ddda6c8b
fanhl <fanhonglin@klicen.com>
2018-05-10 10:35:03 +0800

    upgrade gradle

--- sample/src/main/java/com/fanhl/logviewsample/ui/base/BaseActivity.java
+++ sample/src/main/java/com/fanhl/logviewsample/ui/base/BaseActivity.java
@@ -11,17 +11,20 @@ import com.fanhl.logview.LogViewApi;
  * Created by fanhl on 16/5/5.
  */
 public abstract class BaseActivity extends AppCompatActivity {
-    @Override public void setContentView(@LayoutRes int layoutResID) {
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
         super.setContentView(layoutResID);
         LogViewApi.bind(this);
     }
 
-    @Override public void setContentView(View view) {
+    @Override
+    public void setContentView(View view) {
         super.setContentView(view);
         LogViewApi.bind(this);
     }
 
-    @Override public void setContentView(View view, ViewGroup.LayoutParams params) {
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
         super.setContentView(view, params);
         LogViewApi.bind(this);
     }
